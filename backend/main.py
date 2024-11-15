435881f9667a3d593780fa5372f2930fbc6e1f74
RaduTugui <radu.tugui03@e-uvt.ro>
2024-11-15 18:47:45 +0200

    Add the index page to the backend

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/prajitura', methods=['GET'])
 def say_hi():
     return jsonify({"message":"HI!"}),200
+
+@app.route('/prajitura/index')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
