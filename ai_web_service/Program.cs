2d389c9bd0812632d8310d95defc40e1f5cf19a2
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-11-26 13:18:02 +0200

    added kernel memory lib in kernel-memory folder

--- ai_web_service/Program.cs
+++ ai_web_service/Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 app.UseRouting();
+IKernelMemory memory = null;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddConsole()
+        .AddDebug()
+        .SetMinimumLevel(LogLevel.Debug);
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
 
 async void initialize()
 {
@@ -26,7 +37,7 @@ async void initialize()
         EmbeddingModel = new OllamaModelConfig("mxbai-embed-large:latest")
     };
 
-    var memory = new KernelMemoryBuilder()
+    memory = new KernelMemoryBuilder()
         .WithOllamaTextGeneration(config, new GPT4oTokenizer())
         .WithOllamaTextEmbeddingGeneration(config, new GPT4oTokenizer())
         .Configure(builder => builder.Services
@@ -40,19 +51,34 @@ async void initialize()
     await memory.ImportTextAsync("My name is Sunlless the actual goat!");
 
     var answer = await memory.AskAsync("What is my name?");
-    Console.WriteLine(answer.Result);
+    logger.LogInformation(answer.Result);
 }
 
 app.MapGet("/health-check", () => "Healthy as a horse!");
 
-app.MapPost("/upload", (HttpContext context) =>
+app.MapPost("/upload", async (HttpContext context) =>
 {
-    if (!context.Request.HasFormContentType || context.Request.Form.Files["file"] == null)
+    if (!context.Request.HasFormContentType || context.Request.Form.Files.Count == 0)
     {
         return Results.BadRequest("No file uploaded or incorrect form field name.");
     }
-    IFormFile file = context.Request.Form.Files["file"];
-    Console.WriteLine("The file uploaded is: " + file.FileName);
+    IFormFile[] files = context.Request.Form.Files.ToArray<IFormFile>();
+    foreach(IFormFile file in files)
+        logger.LogInformation("The file uploaded is: " + file.FileName);
+    foreach (IFormFile file in files)
+        try
+        {
+            MemoryStream memStream = new MemoryStream();
+            file.CopyTo(memStream);
+            await memory.ImportDocumentAsync(new Document
+            {
+                Content = memStream.ToArray()
+            },null);
+        }
+        catch(Exception ex)
+        {
+            return Results.StatusCode(501);
+        }
     return Results.Ok();
 }).DisableAntiforgery();
 
