5eba28561a21fb8f14f6e6019aa33943115cde85
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-11-27 11:23:21 +0200

    Added pydanic lib for request body validation

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, render_template, redirect, url_for, flash, jso
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
+from pydantic import BaseModel
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+
+# app.secret_key = 'your_secret_key'
 # pentru toate rutele cu /prajitura/ceva -> poate din uri u ala sa-l acceseze. musai rutele cu /prajitura
 CORS(app, resources={r"/prajitura/*": {"origins": "http://localhost:5173"}})
 
@@ -28,19 +30,24 @@ def upload(searched_url, content):
 def say_hi():
     return jsonify({"message": "HI!"}), 200
 
-
-# @app.route('/prajitura/index', methods=['GET'])
-# def index():
-#     return render_template('index.html')
-
+class SignupModel(BaseModel):
+    username: str
+    email: str
+    password: str
 
 @app.route('/prajitura/signup', methods=['POST'])
 def signup():
-    print(request.get_json())
-    request_body = request.get_json()
-    username = request_body['username']
-    email = request_body['email']
-    password = request_body['password']
+
+    try:
+        # **ia key-value pairurile din dict si le paseaza ca key=val functiei SignupModel
+        request_body = SignupModel(**request.get_json())
+    except Exception as e:
+        return jsonify(e),400
+    
+    #acum request body are garantat fieldurile
+    username = request_body.username
+    email = request_body.email
+    password = request_body.password
     hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
     conn = sqlite3.connect('signup.db')
@@ -56,31 +63,36 @@ def signup():
     conn.commit()
     conn.close()
 
-    return jsonify({"msg": "Nice pass"}) ,200
+    return jsonify({"msg": "User registered successfully"}) ,200
 
-@app.route('/prajitura/login', methods=['GET', 'POST'])
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+@app.route('/prajitura/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    try:
+        request_body = LoginModel(**request.get_json())
+    except Exception as e:
+        return jsonify(e),400
 
-        conn = sqlite3.connect('signup.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-        user = cursor.fetchone()
+    username = request_body.username
+    password = request_body.password
 
-        if user and check_password_hash(user[3], password):
-            return redirect(url_for('index'))
-        else:
-            flash('Invalid username or password!', 'error')
+    conn = sqlite3.connect('signup.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+    user = cursor.fetchone()
 
-    return render_template('login.html')
+    if user and check_password_hash(user[3], password):
+        return jsonify({"message": "Logged in successfully!"}), 200
+    else:
+        return jsonify({"message": "Invalid username or password!"}), 401
 
-@app.route('/prajitura/logout')
+@app.route('/prajitura/logout', methods=['GET'])
 def logout():
     session.pop('username', None)
-    flash('You have been logged out.', 'success')
-    return redirect(url_for('login'))
+    return jsonify({"message": "You have been logged out."}), 200
 
 if __name__ == '__main__':
     init_db()
